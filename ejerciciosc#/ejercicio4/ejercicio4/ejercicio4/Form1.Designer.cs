276c4a67818eb0e273cd474b377ad8180cb674fc
LizzerPH <80063886+LizzerPH@users.noreply.github.com>
2021-04-12 15:54:12 -0500

    ejercicio4

---
+++ ejerciciosc#/ejercicio4/ejercicio4/ejercicio4/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_aceptar = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.txt_entrada = new System.Windows.Forms.TextBox();
+            this.lista_txt = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_aceptar, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txt_entrada, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lista_txt, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_limpiar, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(30, 63);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(749, 348);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btn_aceptar
+            // 
+            this.btn_aceptar.Location = new System.Drawing.Point(377, 3);
+            this.btn_aceptar.Name = "btn_aceptar";
+            this.btn_aceptar.Size = new System.Drawing.Size(369, 168);
+            this.btn_aceptar.TabIndex = 0;
+            this.btn_aceptar.Text = "ACEPTAR";
+            this.btn_aceptar.UseVisualStyleBackColor = true;
+            this.btn_aceptar.Click += new System.EventHandler(this.btn_aceptar_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(377, 177);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(369, 168);
+            this.btn_limpiar.TabIndex = 1;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // txt_entrada
+            // 
+            this.txt_entrada.Location = new System.Drawing.Point(3, 3);
+            this.txt_entrada.Name = "txt_entrada";
+            this.txt_entrada.Size = new System.Drawing.Size(368, 23);
+            this.txt_entrada.TabIndex = 2;
+            // 
+            // lista_txt
+            // 
+            this.lista_txt.FormattingEnabled = true;
+            this.lista_txt.ItemHeight = 15;
+            this.lista_txt.Location = new System.Drawing.Point(3, 177);
+            this.lista_txt.Name = "lista_txt";
+            this.lista_txt.Size = new System.Drawing.Size(368, 154);
+            this.lista_txt.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn_aceptar;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.TextBox txt_entrada;
+        private System.Windows.Forms.ListBox lista_txt;
+    }
+}
+
