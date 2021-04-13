ac90989099994bc77e462668cc39b0aa79c8f288
LizzerPH <80063886+LizzerPH@users.noreply.github.com>
2021-04-13 00:44:42 -0500

    practica2

---
+++ practicas/practicasC#/practica2/practica2/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace practica2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contenedor = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.contenedor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contenedor
+            // 
+            this.contenedor.BackColor = System.Drawing.Color.MistyRose;
+            this.contenedor.Location = new System.Drawing.Point(2, 2);
+            this.contenedor.Name = "contenedor";
+            this.contenedor.Size = new System.Drawing.Size(765, 369);
+            this.contenedor.TabIndex = 0;
+            this.contenedor.TabStop = false;
+            this.contenedor.Paint += new System.Windows.Forms.PaintEventHandler(this.contenedor_Paint);
+            this.contenedor.MouseClick += new System.Windows.Forms.MouseEventHandler(this.contenedor_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 372);
+            this.Controls.Add(this.contenedor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.contenedor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox contenedor;
+    }
+}
+
---
+++ practicas/practicasC#/practica2/practica2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica2
+{
+    public partial class Form1 : Form
+    {
+        float x, y;
+        float w, h;
+        String texto ="Boton 1";
+        public void setX(float x)
+        {
+            this.x = x;
+        }
+        public void setY(float y)
+        {
+            this.y = y;
+        }
+        public void setTexto(String y)
+        {
+            this.texto = y;
+        }
+
+        private void contenedor_MouseClick(object sender, MouseEventArgs e)
+        {
+            setX(e.X);
+            setY(e.Y);
+            setTexto("Moviendo a: [" + e.X + ", " + e.Y + "]");
+            contenedor.Invalidate();
+        }
+
+        public Form1()
+        {
+            w = h= 50;
+            InitializeComponent();
+        }
+
+        private void contenedor_Paint(object sender, PaintEventArgs e)
+        {
+            float c = 12;
+            SolidBrush Green = new SolidBrush(Color.Green);
+            SolidBrush black = new SolidBrush(Color.Black);
+            SolidBrush white = new SolidBrush(Color.White);
+            Graphics g = e.Graphics;
+            g.FillEllipse(Green, 30,300, 50, 50);
+            g.DrawString(texto,new Font("Bold",c), black, new Point(29, 280));
+            g.FillRectangle(white,x,y,w,h);
+            g.FillEllipse(black, x+5, y, 20, 20);
+            g.FillEllipse(black, x +30, y, 20, 20);
+
+        }
+    }
+}
