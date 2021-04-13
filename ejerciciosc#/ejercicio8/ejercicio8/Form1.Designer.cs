cf3eb66d2a5696acd3f3068b91f790fd6996738f
LizzerPH <80063886+LizzerPH@users.noreply.github.com>
2021-04-12 20:40:40 -0500

    ejercicio8

---
+++ ejerciciosc#/ejercicio8/ejercicio8/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace ejercicio8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.area_pintable = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.area_pintable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // area_pintable
+            // 
+            this.area_pintable.Location = new System.Drawing.Point(5, 2);
+            this.area_pintable.Name = "area_pintable";
+            this.area_pintable.Size = new System.Drawing.Size(873, 253);
+            this.area_pintable.TabIndex = 0;
+            this.area_pintable.TabStop = false;
+            this.area_pintable.Paint += new System.Windows.Forms.PaintEventHandler(this.area_pintable_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 261);
+            this.Controls.Add(this.area_pintable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.area_pintable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox area_pintable;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ejerciciosc#/ejercicio8/ejercicio8/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio8
+{
+    public partial class Form1 : Form
+    {
+        Image megaman=null;
+        int x, y;
+        int figura=0;
+        int fx, fy;
+        bool tipo = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            figura++;
+            if (figura>=3)
+            {
+                figura = 0;
+            }
+            if (tipo) fy = 1;
+            else
+                fy = 0;
+            area_pintable.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine(e.KeyChar.ToString());
+            if (e.KeyChar == 'a') { 
+                x--;
+                tipo = true;
+             }
+             if(e.KeyChar == 'd'){
+                x++;
+                tipo = true;
+             }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine("Liberando");
+            tipo = false;
+        }
+
+        public Form1()
+        {
+            x = y = 0;
+            fx = fy = 0;
+            tipo = false;
+            InitializeComponent();
+            megaman = Image.FromFile("Recursos\\imagen.png");
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void area_pintable_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(megaman,
+                new Rectangle(x,y,250,250),
+                fx+figura*250,fy,250,255,GraphicsUnit.Pixel);
+        }
+    }
+}
