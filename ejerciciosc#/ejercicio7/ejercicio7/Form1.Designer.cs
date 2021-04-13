e1159cfb57bc6606bb1242f2fd344d6241d1cdf5
LizzerPH <80063886+LizzerPH@users.noreply.github.com>
2021-04-12 19:35:22 -0500

    ejercicio7

---
+++ ejerciciosc#/ejercicio7/ejercicio7/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace ejercicio7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.area_pintable = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.area_pintable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // area_pintable
+            // 
+            this.area_pintable.Location = new System.Drawing.Point(2, 0);
+            this.area_pintable.Name = "area_pintable";
+            this.area_pintable.Size = new System.Drawing.Size(832, 355);
+            this.area_pintable.TabIndex = 0;
+            this.area_pintable.TabStop = false;
+            this.area_pintable.Paint += new System.Windows.Forms.PaintEventHandler(this.area_pintable_Paint);
+            this.area_pintable.MouseClick += new System.Windows.Forms.MouseEventHandler(this.area_pintable_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 353);
+            this.Controls.Add(this.area_pintable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.area_pintable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox area_pintable;
+    }
+}
+
---
+++ ejerciciosc#/ejercicio7/ejercicio7/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio7
+{
+    public partial class Form1 : Form
+    {
+        float x, y;
+        float w = 100, h = 100;
+        public void setX(float x)
+        {
+            this.x = x;
+        }
+        public void setY(float y)
+        {
+            this.y = y;
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void area_pintable_MouseClick(object sender, MouseEventArgs e)
+        {
+            setX(e.X);
+            setY(e.Y);
+            area_pintable.Invalidate();
+        }
+
+        private void area_pintable_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush GreenBrush = new SolidBrush(Color.Green);
+            SolidBrush BlackBrush = new SolidBrush(Color.Black);
+            SolidBrush RedBrush = new SolidBrush(Color.Red);
+            Graphics g = e.Graphics;
+            g.FillEllipse(GreenBrush, x, y, w, h);
+            g.FillEllipse(BlackBrush, x + 10, y + 15, 30, 30);
+            g.FillEllipse(BlackBrush, x + 60, y + 15, 30, 30);
+            g.FillEllipse(RedBrush, x*10, y*30, w, h);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'a' || e.KeyChar == 'A')
+            {
+                x += -10;
+            }
+            if (e.KeyChar == 'd' || e.KeyChar == 'D')
+            {
+                x += 10;
+            }
+            if (e.KeyChar == 's' || e.KeyChar == 'S')
+            {
+                y += 10;
+            }
+            if (e.KeyChar == 'w' || e.KeyChar == 'W')
+            {
+                y += -10;
+            }
+            area_pintable.Invalidate();
+        }
+    }
+}
