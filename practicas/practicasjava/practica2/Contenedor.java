4f6b6b00d8a31690ef1fe75ab756372d165ba7c2
LizzerPoolHuchim <74462327+LizzerPoolHuchim@users.noreply.github.com>
2021-04-14 02:44:36 -0500

    practica2

--- practicas/practicasjava/practica2/Contenedor.java
+++ practicas/practicasjava/practica2/Contenedor.java
@@ -13,7 +13,7 @@ public class Contenedor extends Canvas {
     private boolean presionado;
     public Contenedor(){
         this.setBackground(Color.pink);
-        posx=50;
+        posx=350;
         posy=60;
         weight=50;
         height=50;
@@ -21,18 +21,33 @@ public class Contenedor extends Canvas {
     }
 
     public void paint(Graphics graficos){
+        graficos.setColor(Color.RED);
+        graficos.fillRect(10,10,750 , 450);
         graficos.setColor(Color.GREEN);
         graficos.fillOval(50,400,weight, height);
-        graficos.setColor(Color.BLACK);
+        graficos.setColor(Color.WHITE);
         graficos.drawString(texto,50,400);
         graficos.setColor(Color.WHITE);
-        graficos.fillRect(posx, posy, weight, height);
-        graficos.setColor(Color.BLACK);
-        graficos.fillOval(posx+10, posy+5,weight-30, height-30);
-        graficos.fillOval(posx+30, posy+5,weight-30, height-30);
-        graficos.drawString(texto2,posx,posy);
-        
-
+        graficos.fillRect(350, 20, 400, 400);
+        if (posx>=350||posx>=670 && posy>=20||posy>=400) {
+            setTexto1("Imposible salirse del componente temporal");
+            repaint();
+            graficos.setColor(Color.BLUE);
+            graficos.fillRect(posx, posy, weight, height);
+            graficos.setColor(Color.BLACK);
+            graficos.fillOval(posx+10, posy+5,weight-30, height-30);
+            graficos.fillOval(posx+30, posy+5,weight-30, height-30);
+            graficos.drawString(texto2,posx,posy);
+        }else{
+            int x=355;
+            int y= 25;
+            graficos.setColor(Color.BLUE);
+            graficos.fillRect(x, y, weight, height);
+            graficos.setColor(Color.BLACK);
+            graficos.fillOval(x+10, y+5,weight-30, height-30);
+            graficos.fillOval(x+30, y+5,weight-30, height-30);
+            graficos.drawString(texto2,x,y);
+        }
     }
     public void setTexto1(String txt){
         this.texto=txt;
