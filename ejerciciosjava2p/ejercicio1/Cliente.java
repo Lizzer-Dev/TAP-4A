6e62ec1df846268024dd20308833621e3c1cc496
LizzerPH <80063886+LizzerPH@users.noreply.github.com>
2021-04-30 09:56:05 -0500

    2p ejercicio1

--- ejerciciosjava2p/ejercicio1/Cliente.java
+++ ejerciciosjava2p/ejercicio1/Cliente.java
@@ -1,29 +1,45 @@
+package ejercicio1;
 
-public class Cliente{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Cliente {
     Socket socket;
     BufferedReader in;
     PrintWriter out;
-    public Cliente() throws IOException,UknowHpstExeption{
-        socket=new Socket("localhost",5000);
-        in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out=new PrintWriter(socket.getOutputStream());
+    public Cliente() throws UnknownHostException, IOException {
+        socket = new Socket("localhost", 5000);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream());
+    }
 
+    public void write(String mensaje) throws IOException {
+        out.println(mensaje);
+        out.flush();
+        String line = in.readLine();
+        System.out.println(line);
     }
-    public void write(){
-       out.println("Hola");
-       out.flush();
-       String line=in.readLine();
-       System.out.println(line);
-       in.close();
-       out.close();
+    public void bye() throws IOException {
+        in.close();
+        out.close();
     }
-    public static void main(String[] args) {
-        try {
-            Cliente c=new Cliente();
-            c.write();
-        } catch (Exception e) {
-            //TODO: handle exception
-            e.printStackTrace();
+    public static void main(String args[]) {
+        try{
+            Cliente c = new Cliente();
+            String line = "";
+            while(!line.equals("adios"))
+            {
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                line = in.readLine();
+                c.write(line);
+            }   
+            c.bye();
+        }catch(Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
