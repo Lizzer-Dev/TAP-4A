d8e2c7f17035cefda1560a8d7ce385f69736588e
LizzerPH <80063886+LizzerPH@users.noreply.github.com>
2021-05-04 09:00:10 -0500

    chat

--- ejerciciosjava2p/ejerciciochat/Cliente.java
+++ ejerciciosjava2p/ejerciciochat/Cliente.java
@@ -2,7 +2,7 @@ package ejerciciochat;
 import javax.swing.*;
 import java.awt.event.*;
 public class Cliente extends JFrame implements ActionListener {
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
--- ejerciciosjava2p/ejerciciochat/Servidor.java
+++ ejerciciosjava2p/ejerciciochat/Servidor.java
@@ -1,13 +1,55 @@
 package ejerciciochat;
-
+import java.awt.BorderLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 import java.awt.event.*;
+import java.io.IOException;
+import java.net.ServerSocket;
 public class Servidor extends JFrame implements ActionListener {
+    JButton btn_reiniciar,btn_salir;
+    JPanel panelce,panelinf,panelsup;
+    JLabel lbl_titulo,lbl_options,lbl_center;
+    JTextArea area;
+    ServerSocket server;
+    String texto;
+    JScrollPane scroll;
+    public Servidor()throws IOException{
+        super("Servidor");
+        server=new ServerSocket(5000);
+        initComponents();
+    }
+    public void start(){
 
+    }
+    public void initComponents(){
+        start();
+        this.setSize(300, 700);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout(5,5));
+        //PANEL CENTRAL
+        panelce=new JPanel();
+        area=new JTextArea();
+        area.setEditable(false);
+        scroll=new JScrollPane(area);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        panelce.add(scroll);
+        panelsup=new JPanel();
+        panelinf=new JPanel();
+
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         
     }
-    
+    public static void main(String[] args) {
+        Servidor serv=new Servidor();
+    }
 }
