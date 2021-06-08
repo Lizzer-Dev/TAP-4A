a59a5c65b5df976ce124747ca4a73fad4131bd53
LizzerPH <80063886+LizzerPH@users.noreply.github.com>
2021-06-08 12:52:38 -0500

    C#

---
+++ ejerciciosC#2p/SQLApp/SQLApp/Controllers/EstadoController.cs
@@ -0,0 +1,67 @@
+﻿using SQLApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLApp.Services;
+namespace SQLApp.Controllers
+{
+    public class EstadoController
+    {
+        Estado estado;
+        List<Estado> list;
+        public List<Estado> listar(){
+            list = EstadoService.GetAll();
+            return list;
+        }
+        public void guardar(Estado e)
+        {
+            try
+            {
+                if (validate(e))
+                {
+                    var estado = list.Find(item => item.Id == e.Id);
+                    if (estado == null)
+                        EstadoService.Insert(e);
+                    else
+                        throw new Exception("El ID ya fue utilizado en otro registro");
+                }
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void actualizar(Estado e)
+        {
+            var estado = list.Find(item => item.Id == e.Id);
+            if (estado != null)
+            {
+                estado.Name = e.Name;
+                estado.Surface = e.Surface;
+            }
+            else
+            {
+
+            }
+            try
+            {
+                if (validate(estado)) _ = EstadoService.Update(estado);
+            }
+            catch (Exception es)
+            {
+                throw es;
+            }
+        }
+
+        private bool validate(Estado e)
+        {
+            if (e.Id <= 0)
+                throw new Exception("ID id Mandatory");
+            if (e.Name.Length == 0)
+                throw new Exception("Name is Mandatory");
+            if (e.Surface < 0)
+                throw new Exception("Surface is Mandatory");
+            return true;
+        }
+    }
+}
---
+++ ejerciciosC#2p/SQLApp/SQLApp/Services/EstadoService.cs
@@ -0,0 +1,74 @@
+﻿using SQLApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Text;
+
+namespace SQLApp.Services
+{
+    public class EstadoService
+    {
+        public const string tname = "ESTADOS";
+
+        public static List<Estado> GetAll()
+        {
+            var result = new List<Estado>();
+            using (System.Data.SQLite.SQLiteConnection ctx = DbContext.GetInstance())
+            {
+                var query = "SELECT * FROM " + tname;
+                using (var command = new SQLiteCommand(query, ctx))
+                {
+                    try
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                result.Add(new Estado
+                                {
+                                    Id = Convert.ToInt32(reader["id"].ToString()),
+                                    Name = reader["name"].ToString(),
+                                    Surface = Convert.ToDouble(reader["surface"].ToString())
+                                });
+
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.ToString();
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static Boolean Insert(Estado estado)
+        {
+            using ( var ctx = DbContext.GetInstance())
+            {
+                var query = string.Format("INSERT INTO {0} (id, name, surface) VALUES({1},'{2}', {3})", tname, estado.Id,estado.Name,estado.Surface);
+                using (var command=new SQLiteCommand(query,ctx))
+                {
+                    int v = command.ExecuteNonQuery();
+                    if (v > 0) return true;
+                    else return false;
+                }
+            }
+        }
+
+        public static Boolean Update(Estado estado)
+        {
+            using ( var ctx = DbContext.GetInstance())
+            {
+                var query = string.Format("UPDATE {0} SET name='{1}', surface={2}", tname, estado.Name, estado.Surface);
+                using (var command=new SQLiteCommand(query,ctx))
+                {
+                    int v = command.ExecuteNonQuery();
+                    if (v > 0) return true;
+                    else return false;
+                }
+            }
+        }
+    }
+}
